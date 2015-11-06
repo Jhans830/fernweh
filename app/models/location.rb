0af659764cd2b129c88c2a759714580e2abeb52d
Jason Hanshaw <jhans830@gmail.com>
2015-11-06 14:18:21 -0600

    Updated README.rdoc

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,10 +2,6 @@ class Location < ActiveRecord::Base
   attr_accessible :address, :activity, :description, :latitude, :longitude
   geocoded_by :address
   after_validation :geocode
-
-def gmaps4rails_infowindow
-    "<b>#{location.address}</b><br /><i>#{location.description}</i><br/><br/>#{location.activity}<br />"
-  end
      
 end
 
